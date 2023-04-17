45b52963d01de05afdf5a5068b50de7eafe78a4d
jaehyeok choi <jaethem8@jaehyeoks-MacBook-Pro.local>
2023-04-17 10:25:36 -0500

    ok

--- test.py
+++ test.py
@@ -3,18 +3,17 @@ import requests
 import time
 
 def ping():
-    for i in range(100):
-        requests.get("http://127.0.0.1:8000/audit/data_dict")
-        requests.get("http://127.0.0.1:8080/audit/CALL8786")
-        requests.get("http://127.0.0.1:8080/audit/RCFA5311")
-        requests.get("http://127.0.0.1:8080/audit/RCFD0090")
-        requests.get("http://127.0.0.1:8080/audit/RCFD0081")
-        print(i)
+    res = requests.get("http://127.0.0.1:8000/audit/data_dict")
+    res = requests.get("http://127.0.0.1:8000/audit/CALL8786")
+    res = requests.get("http://127.0.0.1:8000/audit/RCFA5311")
+    res = requests.get("http://127.0.0.1:8000/audit/RCFA8274")
+    res = requests.get("http://127.0.0.1:8000/audit/RCFAA224")
+
 
 
 def main():
     threads = []
-    for i in range(10):
+    for i in range(35):
         thread = threading.Thread(target=ping)
         threads.append(thread)
     start = time.perf_counter()
